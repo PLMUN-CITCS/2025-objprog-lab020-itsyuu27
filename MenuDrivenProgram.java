2aad3dbd4b0a654a2b6a6702cd6f01510e614eaa
itsyuu27 <mateoyuanfrancis_bscs@plmun.edu.ph>
2025-02-26 19:52:25 +0800

    Activity 20

---
+++ MenuDrivenProgram.java
@@ -0,0 +1,74 @@
+import java.util.Scanner;
+
+public class MenuDrivenProgram
+{
+
+    public static void displayMenu()
+    {
+        System.out.println("Menu: ");
+        System.out.println("1. Greet User");
+        System.out.println("2. Check Even/Odd");
+        System.out.println("3. Exit");
+        System.out.print("Enter your choice (1-3): ");
+    }
+
+    public static void greetUser()
+    {
+        System.out.println("Hello! Welcome!");
+    }
+
+    public static void checkEvenOrOdd()
+    {
+        Scanner input = new Scanner(System.in);
+        int number;
+
+        System.out.print("Enter an integer: ");
+        number = input.nextInt();
+
+        if(number % 2 == 0)
+        {
+            System.out.println(number + " is an Even number.");
+        }
+        else
+        {
+            System.out.println(number + " is an Odd number.");
+        }
+
+        input.close();
+    }
+
+    public static boolean handelMenuChoice(int choice)
+    {
+        switch (choice) 
+        {
+            case 1:
+                greetUser();
+                return true;
+            case 2:
+                checkEvenOrOdd();
+                return true;
+            case 3:
+                System.out.println("Exiting program. Goodbye!");
+                return false;
+            default:
+                System.out.println("Invalid input, input must be 1 to 3.");
+                return true;
+        }
+    }
+
+    public static void main(String[] args) 
+    {
+        Scanner input = new Scanner(System.in);
+        int choice;
+        boolean continueLoop = true;
+
+        do
+        {
+            displayMenu();
+            choice = input.nextInt();
+            continueLoop = handelMenuChoice(choice);
+        }while(continueLoop);
+
+        input.close();
+    }    
+}
